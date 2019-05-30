0aae07fa9cd5f474f082e18f33055ec8d907b4df
adaxing <xing2@ualberta.ca>
2019-05-29 23:58:15 -0600

    start first assignment

---
+++ assignmnt1.rb
@@ -0,0 +1,16 @@
+some_var = 'false'
+another_var = 'nil'
+
+if some_var == 'pink elepant'
+	puts "Don't think about the pink elepant"
+
+elsif another_var.nil?
+	puts "Question mark in the method name"
+
+elsif some_var == false
+	puts "Looks like this one should execute"
+
+else
+	puts "I guess nothing matched... But why?"
+
+end
